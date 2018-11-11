9b3287b4eb77ca61643fb631c073ed94433fd9bf
myj899 <myj899@gmail.com>
2018-11-11 10:46:44 -0500

    Caching issue fixed

--- flask-app/static/src/index.jsx
+++ flask-app/static/src/index.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div>
         HELLO WORLD
-        <div>JUST CHANGE</div>
+        <div>CHANGEasdasdasdads?????</div>
       </div>
     );
   }
